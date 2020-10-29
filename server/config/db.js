9fede8abece316cf814ba7819a12721217ff8147
John Doe <marco.pastore63@gmail.com>
2020-10-28 21:59:54 -0400

    FinishedMidterm4

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book_store2"
+  //"URI": "mongodb://localhost/book_store2"
+  "URI":"mongodb+srv://MarcoP:pmIrzB2jPSq6CiPa@mongodatabaseserver.umfss.mongodb.net/book_store2?retryWrites=true&w=majority"
 };
